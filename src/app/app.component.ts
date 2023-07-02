1881ec30d9a817f37f38e2e6d15344d2a2aa4b6a
Jonas Wagner <jonas.wagner@informatik.hs-fulda.de>
2023-07-02 13:33:13 +0200

    Add german and english language switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { englishContent, germanContent } from './language-content';
+
+interface LanguageContent {
+  [key: string]: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'choan_portfolio';
+  title = 'Jonas\' Portfolio';
+
+  currentLanguageContent: any;
+  englishContent: LanguageContent = englishContent;
+  germanContent: LanguageContent = germanContent;
+
+  constructor() {
+    this.currentLanguageContent = this.englishContent;
+  }
+
+  switchLanguage(language: string) {
+    console.log('switching language to', language);
+    if (language === 'german') {
+      this.currentLanguageContent = this.germanContent;
+      this.replacePlaceholders();
+    } else if (language === 'english'){
+      this.currentLanguageContent = this.englishContent;
+      this.replacePlaceholders();
+    } else {
+      console.log('Language not supported');
+    }
+  }
+
+  replacePlaceholders() {
+    const placeholders: NodeListOf<HTMLElement> = document.querySelectorAll("[data-placeholder]");
+    placeholders.forEach((placeholder: HTMLElement) => {
+      const key = placeholder.getAttribute("data-placeholder");
+      if (key && this.currentLanguageContent[key]) {
+        placeholder.innerHTML = this.currentLanguageContent[key];
+      }
+    });
+  }
+  
+
+  // ngOnInit() {
+  //   this.switchLanguage('german');
+  //   this.replacePlaceholders();
+  // }
+  
 }
+
+
